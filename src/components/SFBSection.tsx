db34b49734e6e44563cae83701f95a953e90ef70
Felipe silva <lipesx07@gmail.com>
2025-11-23 12:56:48 -0300

    feat: adding new card

--- src/components/SFBSection.tsx
+++ src/components/SFBSection.tsx
@@ -1,5 +1,7 @@
 import Book from "../assets/img/book.png";
-
+import trophy from "../assets/img/trophy.png";
+import books from "../assets/img/books.png"
+ 
 export function SDBSection() {
   return (
     <section className="px-20 py-17 dark:bg-gray-900 transition-colors duration-500">
@@ -8,6 +10,7 @@ export function SDBSection() {
           <h1 className="font-introrust text-background dark:text-white text-6xl py-10 tracking-normal leading-tight">
             O que é o sistema de ensino Farias Brito?
           </h1>
+
           <div className="font-montserrat text-background dark:text-gray-100 text-3xl font-bold mt-6 tracking-normal leading-tight">
             <h2>
               O Sistema de Ensino Farias Brito é uma estrutura educacional
@@ -25,9 +28,34 @@ export function SDBSection() {
           <img
             src={Book}
             alt="Livro Sistema Farias Brito"
-            aria-label="Livro Sistema Farias Brito"
             className="w-170 h-auto"
           />
+        </div>
+      </div>
+      <div className="w-full bg-[#FFFFFF] mt-14 rounded-xl p-8 flex justify-center">
+        <div className="flex items-center gap-32">
+          <div className="flex items-center gap-4">
+            <img
+              src={trophy}
+              alt="trophy"
+              className="w-34 h-34 object-contain"
+            />
+            <p className="font-montserrat text-2xl font-bold text-black">
+                Excelência comprovada.
+            </p>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <img
+              src={books} 
+              alt="books"
+              className="w-34 h-34 object-contain"
+            />
+            <p className="font-montserrat text-2xl font-bold text-black">
+              Metodologia moderna e eficaz.
+            </p>
+          </div>
+
         </div>
       </div>
     </section>
